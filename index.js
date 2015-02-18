62c235f6786455a3ac7da62ff7b2cc61db2b5a90
Timothy Gu <timothygu99@gmail.com>
2015-02-18 14:22:29 -0800

    Don't use `render`, use `compile` as that's what available upstream

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ var eco = require('eco');
 
 exports.name = 'eco';
 exports.outputFormat = 'xml';
-exports.render = function (str, options) {
-  return eco.render(str, options);
-};
+exports.compile = eco;
